cdde9f58dcb0c106a83d494c3bfcc5f5df9ad790
Vishal Doshi <vishal.doshi@gmail.com>
2023-05-18 02:19:46 +0530

    Switch to es-module instead of commonjs

--- bin/open.js
+++ bin/open.js
@@ -1,52 +1,58 @@
 #!/usr/bin/env node
 
-const openBrowser = require("react-dev-utils/openBrowser");
-const chalk = require("chalk");
-const argv = require("minimist")(process.argv.slice(2), { boolean: [ 'n', 'dry-run' ], alias: { 'n': 'dry-run' }});
+import openBrowser from "react-dev-utils/openBrowser.js";
+import chalk from "chalk";
+import minimist from "minimist";
 
-let uri;
+const argv = minimist(process.argv.slice(2), { boolean: ['n', 'dry-run'], alias: { 'n': 'dry-run' } });
 
-//Usage?
-if (argv["?"] || argv.h || argv.help) {
-  usage();
+main();
+
+function main() {
+  let uri;
+
+  //Usage?
+  if (argv["?"] || argv.h || argv.help) {
+    usage();
+    return 0;
+  }
+
+  if (argv._.length == 0) {
+    error_log("No URL specified");
+    usage();
+    return 1;
+  }
+
+  let arg = argv._.join();
+  if (!arg.match(/^https?:\/\//)) {
+    arg = "http://" + arg;
+  }
+
+  //Valid inputs?
+  try {
+    uri = new URL(arg);
+  } catch (e) {
+    error_log(`Could not parse ${arg} as a URL`);
+    usage();
+    return 1;
+  }
+
+  //Is dry run?
+  if (argv.n) {
+    process.env.BROWSER = "none";
+  }
+
+  //Let's open...
+  let opened = openBrowser(uri);
+  if (!opened && !argv.n) {
+    error_log(`Could not open: "${uri}"`);
+    return 1;
+  } else {
+    console.log(chalk.blue(`${argv.n ? "Dry-run" : "Launched"}: ${uri}`));
+  }
+
   return 0;
 }
-
-if (argv._.length == 0) {
-  error_log("No URL specified");
-  usage();
-  return 1;
-}
-
-let arg = argv._.join();
-if (!arg.match(/^https?:\/\//)) {
-  arg = "http://" + arg;
-}
-
-//Valid inputs?
-try {
-  uri = new URL(arg);
-} catch (e) {
-  error_log(`Could not parse ${arg} as a URL`);
-  usage();
-  return 1;
-}
-
-//Is dry run?
-if (argv.n) {
-  process.env.BROWSER = "none";
-}
-
-//Let's open...
-let opened = openBrowser(uri);
-if (!opened && !argv.n) {
-  error_log(`Could not open: "${uri}"`);
-  return 1;
-} else {
-  console.log(chalk.blue(`${argv.n ? "Dry-run": "Launched"}: ${uri}`));
-}
-
-return 0;
 
 function error_log(msg) {
   console.error(chalk.red(msg));
